83273e042850a1da3c8000da38755ef4957303cf
Neimhin <nrobinso@tcd.ie>
2024-03-31 20:07:48 +0100

    init.sql customer name is VARCHAR(255)

--- containers/init.sql
+++ containers/init.sql
@@ -1,23 +1,23 @@
 CREATE TABLE customer (
     region INT,
-    id INT,
+    name VARCHAR(255),
     family_region INT, family_id INT,
     password TEXT,
-    PRIMARY KEY (region, id)
+    PRIMARY KEY (region, name)
 );
 
 CREATE TABLE family (
     region INT,
     id INT,
-    m1_region INT, member1_id INT,
-    m2_region INT, member2_id INT,
-    m3_region INT, member3_id INT,
-    m4_region INT, member4_id INT,
+    m1_region INT, member1_id VARCHAR(255),
+    m2_region INT, member2_id VARCHAR(255),
+    m3_region INT, member3_id VARCHAR(255),
+    m4_region INT, member4_id VARCHAR(255),
     PRIMARY KEY (region, id),
-    FOREIGN KEY (m1_region, member1_id) REFERENCES customer(region, id),
-    FOREIGN KEY (m2_region, member2_id) REFERENCES customer(region, id),
-    FOREIGN KEY (m3_region, member3_id) REFERENCES customer(region, id),
-    FOREIGN KEY (m4_region, member4_id) REFERENCES customer(region, id)
+    FOREIGN KEY (m1_region, member1_id) REFERENCES customer(region, name),
+    FOREIGN KEY (m2_region, member2_id) REFERENCES customer(region, name),
+    FOREIGN KEY (m3_region, member3_id) REFERENCES customer(region, name),
+    FOREIGN KEY (m4_region, member4_id) REFERENCES customer(region, name)
 );
 
 ALTER TABLE customer
@@ -39,9 +39,9 @@ WHERE
 CREATE TABLE purchases (
     region INT, id INT,
     family_region INT, family_id INT,
-    customer_region INT, customer_id INT,
+    customer_region INT, customer_name VARCHAR(255),
     amount_euro_equivalent DECIMAL,
-    FOREIGN KEY (customer_region, customer_id) REFERENCES customer(region, id),
+    FOREIGN KEY (customer_region, customer_name) REFERENCES customer(region, name),
     FOREIGN KEY (family_region, family_id) REFERENCES family(region, id),
     PRIMARY KEY (region, id)
 );
