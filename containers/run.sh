2b3b6207ee36cd9fedeedc551f417ad07971b3f5
Neimhin <nrobinso@tcd.ie>
2024-04-01 23:23:38 +0100

    run.sh to run containers (not working)

--- containers/run.sh
+++ containers/run.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-docker network rm cafe-app-network 2>/dev/null
-docker network create --subnet=172.16.0.0/12 --gateway=172.16.0.1 cafe-app-network
+# docker network rm cafe-app-network 2>/dev/null
+# docker network create --subnet=172.16.0.0/12 --gateway=172.16.0.1 cafe-app-network
 
-docker rm cafe-app-neimhin
+docker rm cafe-app-neimhin 2>/dev/null
 docker run -d -it \
-  --network cafe-app-nework \
+  --network bridge \
   --name cafe-app-neimhin \
   --ip 172.17.0.0 \
   --add-host  neimhin:172.17.0.0 \
   --add-host  cian:172.17.0.1 \
   cafe-app-neimhin
 
-docker rm cafe-app-cian
+docker rm cafe-app-cian 2>/dev/null
 docker run -d -it \
-  --network cafe-app-network \
+  --network bridge \
   --name cafe-app-cian \
   --ip 172.17.0.1 \
   --add-host  neimhin:172.17.0.0 \
